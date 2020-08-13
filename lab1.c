62c8ec4091b1502f0b0175b4e9731679083b6155
decker-dot <56293800+decker-dot@users.noreply.github.com>
2020-08-12 20:17:00 -0700

    Update and rename CTF.c to lab1.c

---
+++ lab1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    printf("table\n");
+    int fahr,celsius;
+    int lower,upper,step;
+    lower = 0;
+    upper = 100;
+    step = 5;
+    celsius = lower;
+    printf("celsius\t,fahr\n");
+    while (celsius <= upper) {
+        fahr=celsius*9/5+32;
+        printf("%d\t%d\n",celsius,fahr);
+        celsius = celsius + step;
+    }
+}
